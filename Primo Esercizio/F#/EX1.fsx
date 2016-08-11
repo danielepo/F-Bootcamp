597d10ce09a7328c45c57e8872076e44b3a96895
danielepo <dnl.pozzobon@gmail.con>
2016-08-11 08:30:08 +0200

    Simplyfy fibonacci

--- Primo Esercizio/F#/EX1.fsx
+++ Primo Esercizio/F#/EX1.fsx
@@ -1,7 +1,7 @@
-let rec fib n =
-    match n with
-    | 1 | 2 -> 1
-    | _ -> (fib (n-1) )+(fib (n - 2)) 
+let rec fib n = 
+    if n = 1 || n = 2 
+    then 1 
+    else (fib (n-1) )+(fib (n - 2)) 
 
 
 printf "%d" (fsi.CommandLineArgs 
