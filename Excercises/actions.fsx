01d134cbb7496a9ec22ef3985fe1d39254aed93c
danielepo <dnl.pozzobon@gmail.com>
2017-05-27 00:16:16 +0200

    Add Sharp Adventure files

---
+++ Excercises/actions.fsx
@@ -0,0 +1,16 @@
+
+#load "./models.fsx"
+#load "./gameWorld.fsx"
+module Actions =
+    open Models
+    open GameWorld
+    
+    let move gameWorld (direction:Direction):Player  =
+        printf "Player Can't do this action"
+        player
+
+    let look gameWorld (direction:Direction)  =
+        printf "Player Can't do this action"
+
+    let openExit gameWorld (exit:Exit) =
+        printf "Player Can't do this action"
---
+++ Excercises/models.fsx
@@ -0,0 +1,47 @@
+module Models
+//
+// --------- Model ---------
+//
+
+type Details =
+    { Name: string
+      Description: string }
+
+type Item =
+    { Details: Details }
+
+type RoomId =
+    | RoomId of string
+
+type Exit =
+    | PassableExit of string * destination: RoomId
+    | LockedExit of string * key: Item * next: Exit 
+    | NoExit of string option
+
+type Exits =
+    { North: Exit
+      South: Exit
+      East: Exit
+      West: Exit }
+
+type Direction =
+    | North
+    | South
+    | East
+    | West
+    | Stay
+
+type Room =
+    { Id: RoomId
+      Details: Details
+      Items: Item list
+      Exits: Exits }
+
+type Player =
+    { Details: Details
+      Location: RoomId
+      Inventory: Item list }
+
+type World =
+    { Rooms: Map<RoomId, Room> 
+      Player: Player }
