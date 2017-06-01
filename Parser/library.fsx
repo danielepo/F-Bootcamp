f5c80c07853b8bf8459d8c6deae720c01b516876
danielepo <dnl.pozzobon@gmail.com>
2017-06-01 22:42:25 +0200

    add combinators

--- Parser/library.fsx
+++ Parser/library.fsx
@@ -84,3 +84,66 @@ let parseABC = pstring "ABC"
 run parseABC "ABCDE"  // Success ("ABC", "DE")
 run parseABC "A|CDE"  // Failure "Expecting 'B'. Got '|'"
 run parseABC "AB|DE"  // Failure "Expecting 'C'. Got '|'"
+
+let rec parseZeroOrMore parser input =
+    let result = run parser input
+    
+    match result with
+    | Failure m -> ([], input)
+    | Success (firstValue, firstRemaining) -> 
+        let (subsequetValues, subsequentRemainig) = parseZeroOrMore parser firstRemaining
+        (firstValue :: subsequetValues, subsequentRemainig)
+
+let many parser =
+    let innerFn input = Success (parseZeroOrMore parser input)
+    Parser innerFn
+
+run (many <| pstring "AB") "ABABdl"
+
+let whitespaceChar = anyOf [' '; '\t'; '\n']
+let whitespace = many whitespaceChar 
+run whitespace "ABC"  // Success ([], "ABC")
+run whitespace " ABC"  // Success ([' '], "ABC")
+run whitespace "\tABC"  // Success (['\t'], "ABC")
+run whitespace "\nABC"  // Success (['\t'], "ABC")
+
+let many1 parser= 
+    let innerFn input = 
+        let result = run parser input
+
+        match result with
+        | Failure m -> Failure m
+        | Success (firstValue,firstRemaining) -> 
+            let (subsequentValues,subsequentRemainig) = parseZeroOrMore parser firstRemaining
+            Success (firstValue::subsequentValues, subsequentRemainig)
+    Parser innerFn
+
+// define parser for one digit
+let digit = anyOf ['0'..'9']
+
+// define parser for one or more digits
+let digits = many1 digit 
+
+
+let pint = 
+    let resultToInt result = 
+        System.String(Array.ofList result) |> int
+    let digit = anyOf ['0'..'9']
+    let digits = many1 digit
+
+    digits
+    |> mapP resultToInt
+
+run pint "1ABC"  // Success (['1'], "ABC")
+run pint "12BC"  // Success (['1'; '2'], "BC")
+run pint "123C"  // Success (['1'; '2'; '3'], "C")
+run pint "1234"  // Success (['1'; '2'; '3'; '4'], "")
+run pint "ABC"   // Failure "Expecting '9'. Got 'A'"
+
+let opt p = 
+    // map the result of a a parser to Some
+    let some = p |>> Some
+    // lift None
+    let none = returnP None
+    // if the combinator orelse to choose none if some fails
+    some <|> none
